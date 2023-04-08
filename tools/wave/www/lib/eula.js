09b51ee47a453b4faa28914127f7309cf89092cf
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2023-04-08 07:29:59 -0400

    add simple modal EULA with local cookie

---
+++ tools/wave/www/lib/eula.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    //localStorage.removeItem('eulaAccepted'); //toggle for debug
+    if (localStorage.getItem('eulaAccepted') != 'yes') {
+        const eulaModel = document.getElementById('eulaDialog');
+        eulaModel.showModal();
+    }
+});
+
+const acceptEulaButton = document.getElementById("acceptEula");
+acceptEulaButton.addEventListener("click", () => {
+    localStorage.setItem('eulaAccepted', 'yes');
+});
